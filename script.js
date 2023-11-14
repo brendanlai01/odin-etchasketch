df64526d51d96b43b66ba9756b35af2859f0eee6
Brendan Lai <brendanlai00@gmail.com>
2023-11-14 03:08:59 -0500

    Revise createGrid function, create buttons

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const container = document.querySelector('#container');
 function createGrid(numRow){
     for(i = 0; i < numRow * numRow; i++){
         let gridDiv = document.createElement('div');
-        gridDiv.style.cssText = 'width: calc(100%/16); height: calc(100/16)%;';
+        gridDiv.style.width = "calc(100%/" + numRow + ")";
+        gridDiv.style.height =  "calc(100%/" + numRow + ")";
         container.appendChild(gridDiv);
     }
 }
 
-createGrid(16);
+createGrid(24);
