3ed025ff23558da287f30dfa77c4bf007d1a71c7
Brendan Lai <brendanlai00@gmail.com>
2023-11-14 02:28:37 -0500

    Rename .jss to .js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('#container');
 function createGrid(numRow){
     for(i = 0; i < numRow * numRow; i++){
         let gridDiv = document.createElement('div');
-        gridDiv.style.cssText = 'width: calc(100%/numRow); height: calc(100/numRow)%;';
+        gridDiv.style.cssText = 'width: calc(100%/16); height: calc(100/16)%;';
         container.appendChild(gridDiv);
     }
 }
