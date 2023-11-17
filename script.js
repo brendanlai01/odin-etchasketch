ec9e7b734d8cdaf256fca9c9009c8b5101ba3d2d
Brendan Lai <brendanlai00@gmail.com>
2023-11-17 14:48:06 -0500

    Add reset method for reset button

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('#container');
+const gridButton = document.querySelector('#changeGrid');
+const resetButton = document.querySelector('#reset');
 
 function createGrid(numRow){
     for(i = 0; i < numRow * numRow; i++){
         let gridDiv = document.createElement('div');
         gridDiv.style.width = "calc(100%/" + numRow + ")";
         gridDiv.style.height =  "calc(100%/" + numRow + ")";
+        gridDiv.setAttribute('id', 'gridSquare');
         addListener(gridDiv);
         container.appendChild(gridDiv);
     }
@@ -15,5 +18,17 @@ function addListener(div){
         div.style.backgroundColor = 'black';
     })
 }
+
+function resetCanvas(){
+    let square = document.querySelectorAll('#container, #gridSquare');
+    
+    square.forEach((item) => {
+        item.style.backgroundColor = 'white';
+    });
+}
+
+resetButton.addEventListener('click', () =>{
+    resetCanvas();
+});
 
 createGrid(16);
